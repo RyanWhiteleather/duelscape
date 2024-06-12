9b7773e6d3fc6526f1833dd4eea063c34dd7add0
Ryan Whiteleather <RyanWhiteleather383@gmail.com>
2024-06-12 16:32:12 -0400

    add proxy to vite config, test api call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
+import {useEffect, useState} from "react";
+
 export default function HomePage() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('/api/WeatherForecast')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Error:', error));
+    }, []);
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-[#000000] via-[#00264d] via-[#003366]  to-[#5d0000] text-white">
+        <main
+            className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-[#000000] via-[#00264d] via-[#003366]  to-[#5d0000] text-white">
             <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
                 <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
                     Epic Duels
                 </h1>
             </div>
+            <h1>API Data</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
         </main>
     );
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` (development, production, etc.)
+  // .env.[mode] will override .env
+  process.env = {...process.env, ...loadEnv(mode, process.cwd(), '')};
+  const port = process.env.PORT || '3000';
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      },
+      port: parseInt(port)
+    }
+  }
 })
+
+
