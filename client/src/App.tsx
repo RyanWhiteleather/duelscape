f36d66125b97aab2179283bc6da4036ce473d110
Ryan Whiteleather <RyanWhiteleather383@gmail.com>
2024-06-12 10:17:56 -0400

    Initial Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-[#000000] via-[#00264d] via-[#003366]  to-[#5d0000] text-white">
+            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+                <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+                    Epic Duels
+                </h1>
+            </div>
+        </main>
+    );
+}
