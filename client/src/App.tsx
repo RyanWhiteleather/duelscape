63d9b0194cd709ca0af4ed9884f559b559472853
Ryan Whiteleather <RyanWhiteleather383@gmail.com>
2024-06-13 10:37:40 -0400

    Build Trigger

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
             className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-[#000000] via-[#00264d] via-[#003366]  to-[#5d0000] text-white">
             <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
                 <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-                    Epic Duels TEST
+                    Epic Duels
                 </h1>
             </div>
             <h1>API Data</h1>
