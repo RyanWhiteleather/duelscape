de1557df7d531a8a101eb599c4822f6e62a78118
Ryan Whiteleather <RyanWhiteleather383@gmail.com>
2024-07-15 09:29:05 -0400

    UI work

--- client/src/components/Menus/Lobby.tsx
+++ client/src/components/Menus/Lobby.tsx
@@ -12,12 +12,12 @@ import { useNavigate } from 'react-router-dom';
 export const Lobby = () => {
     const navigate = useNavigate();
     return (
-        <div className="absolute bg-[#2e2e2e] bg-opacity-50  rounded-[12px] text-whitesmoke min-h-[1000px] max-w-[1000px] shadow-[0px_40px_200px_rgba(0,0,0,0.56)] w-[calc(100%_-_140px)] p-[10px_50px_50px_50px] sm:w-[calc(100%_-_70px)] sm:p-[10px_20px_30px_20px]">
-            <div className="mb-4">
+        <div className="relative bg-[#2e2e2e] bg-opacity-50  rounded-[12px] text-whitesmoke h-96 max-w-[1000px] shadow-[0px_40px_200px_rgba(0,0,0,0.56)] w-[calc(100%_-_140px)] p-[10px_50px_50px_50px] sm:w-[calc(100%_-_70px)] sm:p-[10px_20px_30px_20px]">
+            <div className="bg-gray-700 p-4 rounded-t-[12px] flex items-center justify-between">
                 <HomeButton onClick={() => navigate('/')} />
-            </div>
-            <div className="flex flex-row items-center justify-center">
-                <h1 className="text-white sm:text-[2rem]">Lobby</h1>
+                <div className="flex-1 text-center">
+                    <h1 className="text-white sm:text-[2rem] text-center">Lobby</h1>
+                </div>
             </div>
         </div>
     );
