9a7e2e15b47aded59b0598541a79507c1eced04e
Ryan Whiteleather <ryanwhiteleather383@gmail.com>
2024-07-17 05:39:11 -0400

    Lobby work

--- client/src/components/Menus/Lobby.tsx
+++ client/src/components/Menus/Lobby.tsx
@@ -11,7 +11,18 @@ import { MenuHeader } from './MenuHeader.tsx';
 export const Lobby = () => {
     return (
         <div className="relative bg-[#2e2e2e] bg-opacity-50  rounded-[12px] text-whitesmoke h-96 max-w-[1000px] shadow-[0px_40px_200px_rgba(0,0,0,0.56)] w-[calc(100%_-_140px)] p-[10px_50px_50px_50px] sm:w-[calc(100%_-_70px)] sm:p-[10px_20px_30px_20px]">
-            <MenuHeader></MenuHeader>
+            <MenuHeader>Lobby</MenuHeader>
+            <div className="relative flex items-center justify-center h-64">
+                <div className="flex-1 text-center">
+                    <h1 className="text-white">Left Component</h1>
+                </div>
+
+                <div className="w-px bg-gray-400 h-full"></div>
+
+                <div className="flex-1 text-center">
+                    <h1 className="text-white">Right Component</h1>
+                </div>
+            </div>
         </div>
     );
 };
